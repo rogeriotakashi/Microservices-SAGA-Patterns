15fe1d473e6880a9f914258c50bbcec516caac0a
rogeriotakashi <rogeriotakashi@hotmail.com>
2020-01-20 10:33:39 -0200

    Create: CustomerService, OrderService, EurekaServer projects

---
+++ CustomerService/src/test/java/com/rogerio/saga/choreography/CustomerService/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerio.saga.choreography.CustomerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
