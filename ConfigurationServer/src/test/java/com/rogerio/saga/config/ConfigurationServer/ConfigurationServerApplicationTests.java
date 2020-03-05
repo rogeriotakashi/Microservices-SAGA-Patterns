de861f0e13bb92c987f4500fe16a7d47f4333365
Rogério Takashi Hirata <rogeriotakashi@hotmail.com>
2020-03-05 18:15:28 -0300

    Add ConfigurationServer Service

---
+++ ConfigurationServer/src/test/java/com/rogerio/saga/config/ConfigurationServer/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerio.saga.config.ConfigurationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
