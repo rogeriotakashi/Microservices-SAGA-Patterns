d832e87f8d704bc16dfaa25d60fe74008da5343d
rogeriotakashi <rogeriotakashi@hotmail.com>
2020-03-15 09:56:21 -0300

    Add Order being approved

--- OrderService/src/main/java/com/rogerio/saga/choreography/OrderService/listeners/ApprovedOrderListener.java
+++ OrderService/src/main/java/com/rogerio/saga/choreography/OrderService/listeners/ApprovedOrderListener.java
@@ -5,6 +5,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 
 import com.rogerio.saga.choreography.OrderService.config.KafkaConfig;
+import com.rogerio.saga.choreography.OrderService.services.OrderService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,10 +16,12 @@ public class ApprovedOrderListener {
 	@Autowired
 	KafkaConfig kafkaConfig;
 	
+	@Autowired
+	OrderService orderService;
+	
 	@KafkaListener(topics = "#{kafkaConfig.approveOrderTopic}", groupId = "ApprovedOrderListenerGroup")
 	public void approvedOrderListener(Long orderId) {
-		log.info("Entering Approved Order Listener. Order ID: {}", orderId);
-		
-		
+		log.info("Entering Approved Order Listener. Order ID: {}", orderId);	
+		orderService.approveOrder(orderId);
 	}
 }
